34b0a704877cdb4b01beef907b00518751004c69
sotito <christian.soto9@upr.edu>
2015-11-24 22:36:29 -0500

    fixed banner

--- modules/core/client/controllers/carousel.client.controller.js
+++ modules/core/client/controllers/carousel.client.controller.js
@@ -1,71 +1,71 @@
-'use strict';
-angular.module('core').controller('CarouselDemoCtrl', function ($scope){
-   $scope.myInterval = 2500;
- 
-
-   $scope.slides = [{
-    image: '/modules/core/client/img/brand/banner1.png'
-  }, {
-    image: '/modules/core/client/img/brand/banner2.png'
-  }, {
-    image: '/modules/core/client/img/brand/banner3.png'
-  }, {
-    image: '/modules/core/client/img/brand/banner4.png'
-  }, {
-    image: '/modules/core/client/img/brand/banner5.png'
-  }, {
-    image: '/modules/core/client/img/brand/banner6.png'
-  }, {
-    image: '/modules/core/client/img/brand/banner7.png'
-  }, {
-    image: '/modules/core/client/img/brand/banner8.png'
-  }, {
-    image: '/modules/core/client/img/brand/banner9.png'
-  }/*, {
-    image: 'http://www.portableaircompressorsite.com/wp-content/uploads/2014/12/Makita_MAC700_Big_Bore_2.0_HP_Air_Compressor_Small.jpg',
-    text: 'AIR COMPRESSOR'
-  }, {
-    image: 'http://usa-generator.com/troy_bilt/5550_gen.jpg',
-    text: 'GENERATORS'
-  }, {
-    image: 'http://migweldersdirect.co.za/wp-content/uploads/2013/08/MIG140-150x150.jpg',
-    text: 'WELDERS'
-  }, {
-    image: 'http://compactpowerrents.com/public/content/categories/light-tower-110.jpg',
-    text: 'LIGHT TOWERS'
-  }, {
-    image: 'http://www.addco.com/en/files/2011/12/DH250_AB_15-150x150.jpg',
-    text: 'ARROW BOARD'
-  }, {
-    image: 'http://img2.everychina.com/img/d7/c9/c0a8494775c3c707445818af7b86-150x150c1-7b26/high_strength_lw400k_wheel_loader_earthmoving_machinery_convenient_driving.jpg',
-    text: 'WHEEL LOADERS'
-  }, {
-    image: 'http://compactpowerrents.com/public/content/categories/img_backhoe.jpg',
-    text: 'TRACK EQUIPMENT'
-  }, {
-    image: 'http://compactpowerrents.com/public/content/categories/img_trencher.jpg',
-    text: 'TRENCHER'
-  }, {
-    image: 'http://wesparrental.com/wp-content/uploads/1970/01/wacker-150x150.jpg',
-    text: 'COMPACTION EQUIPMENT'
-  }, {
-    image: 'http://rentalexperts.ca/imgs/rental/101.jpg',
-    text: 'VENTILATION EQUIPMENT'
-  }, {
-    image: 'http://www.waterpumpsdirect.com/product-images/WH20_125.jpg',
-    text: 'PUMPS'
-  }, {
-    image: 'http://maxcdn.toolboxbuzz.com/wp-content/uploads/2013/11/2416-22XC_feature-150x150.jpg',
-    text: 'TOOLS & EQUIPMENT'
-  }, {
-    image: 'http://www.azomining.com/images/Equipments/EquipmentThumbImage_487.gif',
-    text: 'PAINT EQUIPMENT'
-  }, {
-    image: 'http://www.toolsupport.com.ng/wp-content/uploads/2015/10/w2-150x150.jpg',
-    text: 'WELDING EQUIPMENT'
-  }*/];
-});
-
-
-
-
+//'use strict';
+//angular.module('core').controller('CarouselDemoCtrl', function ($scope){
+//   $scope.myInterval = 2500;
+//
+//
+//   $scope.slides = [{
+//    image: '/modules/core/client/img/brand/banner1.png'
+//  }, {
+//    image: '/modules/core/client/img/brand/banner2.png'
+//  }, {
+//    image: '/modules/core/client/img/brand/banner3.png'
+//  }, {
+//    image: '/modules/core/client/img/brand/banner4.png'
+//  }, {
+//    image: '/modules/core/client/img/brand/banner5.png'
+//  }, {
+//    image: '/modules/core/client/img/brand/banner6.png'
+//  }, {
+//    image: '/modules/core/client/img/brand/banner7.png'
+//  }, {
+//    image: '/modules/core/client/img/brand/banner8.png'
+//  }, {
+//    image: '/modules/core/client/img/brand/banner9.png'
+//  }/*, {
+//    image: 'http://www.portableaircompressorsite.com/wp-content/uploads/2014/12/Makita_MAC700_Big_Bore_2.0_HP_Air_Compressor_Small.jpg',
+//    text: 'AIR COMPRESSOR'
+//  }, {
+//    image: 'http://usa-generator.com/troy_bilt/5550_gen.jpg',
+//    text: 'GENERATORS'
+//  }, {
+//    image: 'http://migweldersdirect.co.za/wp-content/uploads/2013/08/MIG140-150x150.jpg',
+//    text: 'WELDERS'
+//  }, {
+//    image: 'http://compactpowerrents.com/public/content/categories/light-tower-110.jpg',
+//    text: 'LIGHT TOWERS'
+//  }, {
+//    image: 'http://www.addco.com/en/files/2011/12/DH250_AB_15-150x150.jpg',
+//    text: 'ARROW BOARD'
+//  }, {
+//    image: 'http://img2.everychina.com/img/d7/c9/c0a8494775c3c707445818af7b86-150x150c1-7b26/high_strength_lw400k_wheel_loader_earthmoving_machinery_convenient_driving.jpg',
+//    text: 'WHEEL LOADERS'
+//  }, {
+//    image: 'http://compactpowerrents.com/public/content/categories/img_backhoe.jpg',
+//    text: 'TRACK EQUIPMENT'
+//  }, {
+//    image: 'http://compactpowerrents.com/public/content/categories/img_trencher.jpg',
+//    text: 'TRENCHER'
+//  }, {
+//    image: 'http://wesparrental.com/wp-content/uploads/1970/01/wacker-150x150.jpg',
+//    text: 'COMPACTION EQUIPMENT'
+//  }, {
+//    image: 'http://rentalexperts.ca/imgs/rental/101.jpg',
+//    text: 'VENTILATION EQUIPMENT'
+//  }, {
+//    image: 'http://www.waterpumpsdirect.com/product-images/WH20_125.jpg',
+//    text: 'PUMPS'
+//  }, {
+//    image: 'http://maxcdn.toolboxbuzz.com/wp-content/uploads/2013/11/2416-22XC_feature-150x150.jpg',
+//    text: 'TOOLS & EQUIPMENT'
+//  }, {
+//    image: 'http://www.azomining.com/images/Equipments/EquipmentThumbImage_487.gif',
+//    text: 'PAINT EQUIPMENT'
+//  }, {
+//    image: 'http://www.toolsupport.com.ng/wp-content/uploads/2015/10/w2-150x150.jpg',
+//    text: 'WELDING EQUIPMENT'
+//  }*/];
+//});
+//
+//
+//
+//
--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,19 +1,13 @@
 'use strict';
 
+var homeController = angular.module('core');
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
+homeController.controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
 
-    
+
     // This provides Authentication context.
     $scope.authentication = Authentication;
-
-     $scope.myInterval = 2500;
- 
-
-   
-
-
 
 
   }
@@ -21,6 +15,58 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
 
 ]);
+
+
+homeController.controller('HomeCarouselController', ['$scope', 'Authentication',
+    function ($scope, Authentication) {
+
+        $scope.myInterval = 2500;
+        $scope.noWrapSlides = false;
+        var slides = $scope.slides = [];
+        var currIndex = 0;
+
+        $scope.addSlide = function(){
+            var idBanner = 1 + currIndex;
+            slides.push({
+                image: '/modules/core/client/img/brand/banner' + idBanner + '.png',
+                id: currIndex++
+            });
+
+        };
+
+        for(var i=0; i<9; i++){
+            $scope.addSlide();
+        }
+
+        //$scope.slides = [{
+        //    image: '/modules/core/client/img/brand/banner1.png'
+        //}, {
+        //    image: '/modules/core/client/img/brand/banner2.png'
+        //}, {
+        //    image: '/modules/core/client/img/brand/banner3.png'
+        //}, {
+        //    image: '/modules/core/client/img/brand/banner4.png'
+        //}, {
+        //    image: '/modules/core/client/img/brand/banner5.png'
+        //}, {
+        //    image: '/modules/core/client/img/brand/banner6.png'
+        //}, {
+        //    image: '/modules/core/client/img/brand/banner7.png'
+        //}, {
+        //    image: '/modules/core/client/img/brand/banner8.png'
+        //}, {
+        //    image: '/modules/core/client/img/brand/banner9.png'
+        //}];
+
+
+
+    }
+
+
+
+]);
+
+
 
 
 
